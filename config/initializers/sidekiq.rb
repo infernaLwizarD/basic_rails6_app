34b24ba7670de0b30d2e611ab38cf533f4ad9787
infernal <nikita.dolgopolov@gmail.com>
2023-02-07 16:24:15 +0300

    rubocop's corrections

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,15 +3,9 @@ require 'sidekiq/web'
 Sidekiq::Web.set :sessions, false
 
 Sidekiq.configure_server do |config|
-  config.redis = {
-    host: ENV['REDIS_HOST'] || 'localhost',
-    port: ENV['REDIS_PORT'] || '6379'
-  }
+  config.redis = { host: ENV['REDIS_HOST'] || 'localhost', port: ENV['REDIS_PORT'] || '6379' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {
-    host: ENV['REDIS_HOST'] || 'localhost',
-    port: ENV['REDIS_PORT'] || '6379'
-  }
+  config.redis = { host: ENV['REDIS_HOST'] || 'localhost', port: ENV['REDIS_PORT'] || '6379' }
 end
