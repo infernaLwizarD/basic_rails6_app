95f2b6bcf3cd2052a67cc7e00b12339c001751eb
infernal <nikita.dolgopolov@gmail.com>
2023-03-19 20:09:44 +0300

    create admin in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def create_admin
+  User.create!(
+    role: 'admin',
+    username: 'admin',
+    password: ENV['DB_PASSWORD'],
+    email: "admin@admin.admin",
+    confirmed_at: Time.zone.now
+  )
+end
+
+create_admin
